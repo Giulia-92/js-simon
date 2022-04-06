e988d119a9f5958c8034154f5d6c0a6ddee9288e
Giulia-92 <cionco.giulia@gmail.com>
2022-04-06 20:37:43 +0200

    js-simon

--- script.js
+++ script.js
@@ -11,15 +11,47 @@
   let array = [];
   let numeri = document.getElementById("Numeri casuali");
   function casuale(){
-    let ncasuali = Math.random() * 100;
+    let ncasuali = Math.floor(Math.random()*100)+1;
+    //console.log(ncasuali);
     return ncasuali
-}
-  while(array.length < 5 ){
-      array.push(casuale())
-     
   }
+  document.getElementById("Numeri casuali").addEventListener("click", function(){
+    while(array.length < 5 ){
+      let numcas = casuale();
+      array.push(numcas);
+    }
+    let text = array.join();
+     //stampare i numeri su html
+  for (i = 0; i < array.length; i++){
+    document.getElementById("numeri").innerHTML = text;
+  }
+  setTimeout (creanumeri,3000)
+  })
+
+
+
+ 
   
-  
+  //setTimeoput("funzione cancella numeri e chiedi valori all'utente"30secondi 30000)
+
+
+function creanumeri(){
+  //cancella i numeri
+  //chiedere i numeri all'utente 
+  //confrontare i numeri 
+  //hai vinto/hai perso
+  prompt('inserisci un numero');
+  let nUtente ='';
+  console.log(nUtente);
+  if (nUtente == array.lenght){
+    prompt("Hai vinto");
+  } else{
+    prompt ("Hai perso");
+  }
+}
+
+
+
 
 
 
