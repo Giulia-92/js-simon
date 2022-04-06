58a846a08f6761edb43b9c83d59d9f405ccd28a0
Giulia-92 <cionco.giulia@gmail.com>
2022-04-06 16:13:41 +0200

    js-simon

---
+++ script.js
@@ -0,0 +1,46 @@
+/*function casuali() {
+    numeri = parseInt(document.getElementById("quanti").value);
+    
+     document.getElementById("numeri").innerHTML = "Numeri generati: " + str;
+     } 
+     else {
+        document.getElementById("numeri").innerHTML = "Inserisci un numero maggiore di 0 e inferiore a 100";
+     }
+  }*/
+  
+  let array = [];
+  let numeri = document.getElementById("Numeri casuali");
+  while(array.length < 5 ){
+      function casuale(){
+          Math.round(math.random() * 101);
+          console.log(casuale);
+          
+      }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //document.getElementById("Numeri casuali").innerHTML
+
+ /* if (n <= 100) {
+    = "";
+    for (i = 0; i < n; i++) {
+         num = Math.round(Math.random()*100 + 1);
+         if (i > 0) {
+           += ", ";
+         }
+         += num;
+     }*/
